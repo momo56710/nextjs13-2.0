1c00da1510657d54edcc8a850e70a7f132f93af2
momo <100686706+momo56710@users.noreply.github.com>
2023-07-01 15:30:19 +0100

    grafbase with next js

---
+++ app/components/Footer.tsx
@@ -0,0 +1,80 @@
+import { footerLinks } from "@/constant";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+Image;
+type ColumnProps = {
+  title: string;
+  Links: Array<string>;
+};
+const FooterColumn = ({ title, Links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <h4 className="flex flex-col gap-2 font-normal">
+      {Links.map((link) => (
+        <Link href={"/"} key={link}>
+          {link}
+        </Link>
+      ))}
+    </h4>
+  </div>
+);
+const Footer = () => {
+  return (
+    <footer className="flex-start footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src={"/logo-purple.svg"}
+            width={115}
+            height={38}
+            alt="flexibble"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexibble is the world's leading community for creatives to share,
+            grow, and get hired.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            Links={footerLinks[0].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              Links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              Links={footerLinks[2].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[3].title}
+            Links={footerLinks[3].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              Links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              Links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            Links={footerLinks[6].links}
+          />
+        </div>
+      </div>
+
+      <div className="flexBetween footer_copywright"><p>@ 2023 flexible. All rights reserved</p>
+      <p className="text-gray"><span className="text-black">1254</span> projects</p></div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,26 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('User',{
+  name : g.string(),
+  email : g.string().unique(),
+  avatarUrl : g.url(),
+  description : g.string().optional(),
+  githubUrl : g.url().optional(),
+  LinkedInUrl: g.url().optional(),
+  projects : g.relation(() => Project).list().optional()
+})
+
+const Project = g.model('Project',{
+  title : g.string(),
+  description : g.string(),
+  image : g.url(),
+  liveSiteUrl : g.url(),
+  githubUrl : g.url(),
+  category : g.string().search(),
+  createdBy : g.relation(() => User)
+
+})
+export default config({
+  schema: g
+ 
+})
